443ff04d7fc4e417aa2281e0737975b5087d1868
katenikolaeva <katienikolaeva@gmail.com>
2021-05-30 19:47:57 -0600

    add dealing cards functionality

--- index.js
+++ index.js
@@ -5,35 +5,91 @@ var suits = ["heart", "diamond", "spade", "club"];
 var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 function createDeck() {
-    var deck = new Array();
-	for(var s = 0; s < suits.length; s++) {
-		for(var r = 0; r < ranks.length; r++) {
-			var card = {Rank: ranks[r], Suit: suits[s]};
-			deck.push(card);
-	    }
-	}
-	return deck;
+  var deck = new Array();
+  for (var s = 0; s < suits.length; s++) {
+    for (var r = 0; r < ranks.length; r++) {
+      var card = { Rank: ranks[r], Suit: suits[s] };
+      deck.push(card);
+    }
+  }
+  return deck;
 }
 
 // Shuffling the deck randomly
 function shuffle(deck) {
-	var deckSize = deck.length;
-	var randNum, tempCard;
+  var deckSize = deck.length;
+  var randNum, tempCard;
 
-	for (var i = deckSize - 1; i != 0; i--) {
-	   // Radnomly selecting a position in the deck
-	   randNum = Math.floor(Math.random() * i);
+  for (var i = deckSize - 1; i != 0; i--) {
+    // Radnomly selecting a position in the deck
+    randNum = Math.floor(Math.random() * i);
 
-	   // Swapping the location of the cards
-	   tempCard = deck[i];
-	   deck[i] = deck[randNum];
-	   deck[randNum] = tempCard;
-	}
-	return deck;
+    // Swapping the location of the cards
+    tempCard = deck[i];
+    deck[i] = deck[randNum];
+    deck[randNum] = tempCard;
+  }
+  return deck;
 }
 
- d = createDeck();
- console.log(d);
- shuffledDeck = shuffle(d);
- console.log(shuffledDeck);
+d = createDeck();
+//console.log(d);
+shuffledDeck = shuffle(d);
+//console.log(shuffledDeck);
 // document.getElementById('dealbtn').addEventListener('click', createDeck)
+
+// function to deal cards
+function dealCards(stock) {
+  for (i = 0; i < 7; i++) {
+    for (pile = i; pile < 7; pile++) {
+      var card = stock.pop();
+      var cardHTML = document.createElement("div");
+      cardHTML.setAttribute("class", "card");
+      cardHTML.setAttribute("data-rank", card.Rank);
+      cardHTML.setAttribute("data-suit", card.Suit);
+      document
+        .getElementsByClassName("tableau-pile")
+        [pile].appendChild(cardHTML);
+    }
+  }
+
+  for (i = 0; i < stock.length; i++) {
+    var cardHTML = document.createElement("div");
+    cardHTML.setAttribute("class", "card");
+    cardHTML.setAttribute("data-rank", stock[i].Rank);
+    cardHTML.setAttribute("data-suit", stock[i].Suit);
+    document.getElementsByClassName("stock-pile")[0].appendChild(cardHTML);
+  }
+  faceUp();
+  return stock;
+}
+
+// function to turn cards face up
+function faceUp() {
+  for (i = 0; i < 7; i++) {
+    var card = document.getElementsByClassName("tableau-pile")[i].lastChild;
+    card.className += " up";
+  }
+}
+
+dealCards(d);
+
+// add a click event listener to the 'deal' btn
+var btn = document
+  .getElementById("dealbtn")
+  .addEventListener("click", function () {
+    clearPiles();
+    d = createDeck();
+    shuffledDeck = shuffle(d);
+    dealCards(d);
+  });
+
+// clear stock and tableau piles
+function clearPiles() {
+  var tableauPiles = document.querySelectorAll(".tableau-pile");
+  var stockPile = document.querySelector(".stock-pile");
+  stockPile.innerHTML = null;
+  Array.prototype.forEach.call(tableauPiles, function(pile) {
+    pile.innerHTML = null;
+  });
+}
