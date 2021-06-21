c4d117665147bc362482c5178d135724805b0e5b
ericd <demo0033@algonquinlive.com>
2021-06-21 14:18:53 -0400

    Added scoring events, game timer, type of scoring. Scoring of unimplemented functionality not added!

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 /* Import or write your JavaScript code here */
 // console.log("Hello, guys!");
 
-var suits = ["heart", "diamond", "spade", "club"];
-var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const suits = ["heart", "diamond", "spade", "club"];
+const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+// scoring types
+const NORMALSCORING = 0, VEGASSCORING = 1;
+// scoring event types
+const SCORETABLEAU = 1, SCORETALON = 2, SCOREFOUNDATION = 3, SCOREGAMEOVER = 4, SCORE10S = 5;
+var scoreType = NORMALSCORING;
 var isSelected = false;
+var startTime = new Date().getTime();
+var tenSecondScoreTimer = function() {addToScore(SCORE10S);}
 
 d = createDeck();
 //console.log(d);
@@ -11,6 +18,7 @@ shuffledDeck = shuffle(d);
 //console.log(shuffledDeck);
 // document.getElementById('dealbtn').addEventListener('click', createDeck)
 dealCards(d);
+resetScore();
 
 function createDeck() {
   var deck = new Array();
@@ -83,7 +91,86 @@ var btn = document
     d = createDeck();
     shuffledDeck = shuffle(d);
     dealCards(d);
+    resetScore();
   });
+
+  // Takes resets current score and updates the startTime timestamp
+function resetScore() {
+  var score = document.querySelector(".score");
+  if(scoreType == VEGASSCORING) {
+    score.innerHTML = "-52";
+  }
+  else { // scoreType == NORMALSCORING
+    score.innerHTML = "0";
+  }
+  startTime = new Date().getTime();
+  tenSecondScoreTimer = setInterval(tenSecondScoreTimer, 10000);
+}
+
+// Alter score by a specific amount based on the score event and scoring type active
+//                              Standard    Vegas
+// 1: tableau card flipped        5
+// 2: card moved from talon       5 
+// 3: card moved to foundation    10          5
+// 4: game ended                 bonus
+// 5: 10s elapsed                 -2
+function addToScore(eventType) {
+  var amount = 0;
+  if(scoreType == NORMALSCORING) {
+    switch(eventType) {
+      case SCORETABLEAU:
+        amount = 5;
+        break;
+      case SCORETALON:
+        amount = 5;
+        break;
+      case SCOREFOUNDATION:
+        amount = 10;
+        break;
+      case SCOREGAMEOVER:
+        // Add [700,000 / (seconds to win the game)] bonus points
+        var gameLength = (new Date().getTime() - startTime) / 1000;
+        amount = Math.round(700000 / gameLength)
+        break;
+      case SCORE10S:
+        amount = -2;
+        break;
+      default:
+        amount = 0;
+    }
+  }
+  else if(scoreType == VEGASSCORING) {
+    switch(eventType) {
+      case SCOREFOUNDATION:
+        amount = 5;
+        break;
+      default:
+        amount = 0;
+    }
+  }
+
+  var score = document.querySelector(".score");
+  score.innerHTML = Number(score.innerHTML) + amount;
+}
+
+function win() {
+  addToScore(SCOREGAMEOVER);
+  console.log("game length (s): " + gameLength);
+}
+
+function checkWin() {
+  var foundations = document.querySelectorAll(".foundations-pile");
+
+  for (let i = 0; i < foundations.length; i++) {
+    if(!foundations[i].lastChild ||
+      foundations[i].lastChild.getAttribute("data-rank") != ranks[12]) {
+        // If the top card on any foundation pile is null or not a king, the game is not over!
+        return false;
+    }
+  }
+  // all foundations' top card is a king
+  return true;
+}
 
 // clear foundations, stock and tableau piles
 function clearPiles() {
@@ -149,6 +236,15 @@ function moveCardToTableau(pile, event) {
       // TODO: possible error feedback (red flash/sound, etc)
     }
     if (validMove) {
+      // Score 10 points when moving to a foundation
+      if(pile.classList.contains("foundations-pile")) {
+        addToScore(SCOREFOUNDATION);
+      }
+      // Score 5 points when moving from the talon pile
+      // if(*card is from parent pile*) {
+      //   addToScore(SCORETALON);
+      // }
+
       // remove cardSelected from the old parent pile
       for (i = 0; i < cardsSelected.length; i++) {
         parentPile.removeChild(cardsSelected[i]);
@@ -156,6 +252,8 @@ function moveCardToTableau(pile, event) {
       // open the last card left in that pile (if any)
       if (parentPile.lastChild) {
         parentPile.lastChild.classList.add("up");
+        // New card turned face up, add 5 to score
+        addToScore(SCORETABLEAU);
       }
       // add cardSelected to the end of the new pile
       for (i = 0; i < cardsSelected.length; i++) {
@@ -169,6 +267,10 @@ function moveCardToTableau(pile, event) {
     }
 
     isSelected = false;
+
+    if(checkWin()) {
+      win();
+    }
   }
 }
 
